71a128377b125b20c6f3c5627fb9ba1dff32fed9
VikasMeshram2708 <meshramvikas744@gmail.com>
2024-12-16 20:04:31 +0530

    :tada: crated qr code generator.

--- components/qr-form.tsx
+++ components/qr-form.tsx
@@ -3,6 +3,7 @@ import React, { FormEvent, useState } from "react";
 import { Input } from "./ui/input";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Button } from "./ui/button";
+import QRCode from "react-qr-code";
 
 export default function QrForm() {
   const [user, setUser] = useState({
@@ -10,6 +11,12 @@ export default function QrForm() {
     age: "",
     phone: "",
   });
+
+  const [qrData, setQrData] = useState<{
+    name: string;
+    age: string;
+    phone: string;
+  } | null>(null);
 
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -23,40 +30,48 @@ export default function QrForm() {
       age: "",
       phone: "",
     });
+    setQrData({ ...qrData, name: user.name, age: user.age, phone: user.phone });
   };
+  console.log("q", JSON.parse(JSON.stringify(qrData)));
   return (
-    <Card className="shadow-lg w-full max-w-xl mx-auto">
-      <CardHeader>
-        <CardTitle className="text-center text-">Fill Your Data</CardTitle>
-      </CardHeader>
-      <CardContent className="p-3">
-        <form onSubmit={onSubmit} className="grid gap-3">
-          <Input
-            value={user.name}
-            onChange={(e) => setUser({ ...user, name: e.target.value })}
-            name="name"
-            type="text"
-            placeholder="Enter Your Name"
-          />
-          <Input
-            name="age"
-            value={user.age}
-            onChange={(e) => setUser({ ...user, age: e.target.value })}
-            type="text"
-            placeholder="Enter Your Age"
-          />
-          <Input
-            value={user.phone}
-            onChange={(e) => setUser({ ...user, phone: e.target.value })}
-            name="phone"
-            type="tel"
-            placeholder="Enter Your Phone Number"
-          />
-          <Button type="submit" variant={"secondary"}>
-            Submit
-          </Button>
-        </form>
-      </CardContent>
-    </Card>
+    <>
+      <Card className="shadow-lg w-full max-w-xl mx-auto">
+        <CardHeader>
+          <CardTitle className="text-center text-">Fill Your Data</CardTitle>
+        </CardHeader>
+        <CardContent className="p-3">
+          <form onSubmit={onSubmit} className="grid gap-3">
+            <Input
+              value={user.name}
+              onChange={(e) => setUser({ ...user, name: e.target.value })}
+              name="name"
+              type="text"
+              placeholder="Enter Your Name"
+            />
+            <Input
+              name="age"
+              value={user.age}
+              onChange={(e) => setUser({ ...user, age: e.target.value })}
+              type="text"
+              placeholder="Enter Your Age"
+            />
+            <Input
+              value={user.phone}
+              onChange={(e) => setUser({ ...user, phone: e.target.value })}
+              name="phone"
+              type="tel"
+              placeholder="Enter Your Phone Number"
+            />
+            <Button type="submit" variant={"secondary"}>
+              Submit
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+
+      <div className="py-10">
+        <QRCode value={JSON.stringify(qrData)} />
+      </div>
+    </>
   );
 }
